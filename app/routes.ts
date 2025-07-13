cc8a774866173625cd905a2b83eb1f8f0af017c2
euliveiras <matheus.skm@hotmail.com>
2025-07-12 21:44:42 -0300

    changes

--- app/routes.ts
+++ app/routes.ts
@@ -3,12 +3,10 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 export default [
   route("register", "./routes/layout.tsx", [
     layout("./routes/register/layout.tsx", [
-      route("user", "./routes/register/user.tsx", [
-        route("form", "./routes/register/user-form.tsx"),
-      ]),
-      route("admin", "./routes/register/admin.tsx", [
-        route("form", "./routes/register/admin-form.tsx"),
-      ]),
+      route("user", "./routes/register/user.tsx"),
+      route("user/form", "./routes/register/user-form.tsx"),
+      route("admin", "./routes/register/admin.tsx"),
+      route("admin/form", "./routes/register/admin-form.tsx"),
     ]),
   ]),
 ] satisfies RouteConfig;
